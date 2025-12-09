07de2f8c01d7ee53b8d67518824a461508ecab5b
Gustavo Souza <gustavosouza.aer@gmail.com>
2025-12-09 08:58:50 -0300

    Initial Commit

---
+++ FitterStronger/FitterStrongerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitterStrongerApp.swift
+//  FitterStronger
+//
+//  Created by Gustavo Medeiros Souza on 09/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FitterStrongerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
